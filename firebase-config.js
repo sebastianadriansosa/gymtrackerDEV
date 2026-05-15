9e3f50fbff3b662ffd4de8c3bd3f6e9920d6e9f8
Sebastian Sosa <ssosa17@gmail.com>
2026-05-15 20:12:51 -0300

    Add files via upload
    separacion de codigos en archivos separados

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBBnDju89oV6vq7R7TxisHEksaOcOq_4ls",
+  authDomain: "gymtracker-dev-b9bf9.firebaseapp.com",
+  projectId: "gymtracker-dev-b9bf9",
+  storageBucket: "gymtracker-dev-b9bf9.firebasestorage.app",
+  messagingSenderId: "690418104394",
+  appId: "1:690418104394:web:bacbc3ed6b4666b9598eb3",
+  measurementId: "G-YKVDKY2G4Z"
+};
